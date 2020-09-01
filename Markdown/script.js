6ee8ee54ae3d9dac3ad247d29e3464082774efad
muxiaochen <muxiaochen@vipkid.com.cn>
2020-09-01 16:14:07 +0800

    markdown notes favroite

--- Markdown/script.js
+++ Markdown/script.js
@@ -24,6 +24,11 @@ new Vue({
             // 返回与selectedId匹配怼笔记
             console.log(this.notes.find(note => note.id === this.selectedId))
             return this.notes.find(note => note.id === this.selectedId)
+        },
+        sortedNotes(){
+            return this.notes.slice()
+                .sort((a,b) => a.created - b.created)
+                .sort((a,b) => (a.favorite === b.favorite) ? 0: a.favorite ? -1 : 1 )
         }
     },
 
@@ -91,7 +96,9 @@ new Vue({
             }
         },
         favoriteNote(){
-            
+            //   this.selectedNote.favorite = !this.selectedNote.favorite
+            // this.selectedNote.favorite = this.selectedNote.favorite ^ true
+            this.selectedNote.favorite ^= true  
         }
     },
 
