918e8a3147de6801ecc131eb93d00efa8805693b
Rachelmu <631032057@qq.com>
2020-09-07 15:19:36 +0800

    templages

--- CardGame/components/world.js
+++ CardGame/components/world.js
@@ -37,4 +37,98 @@ Vue.component('castle-banners', {
         return this.player.health / maxHealth
       },
     }
-  })
+})
+
+
+Vue.component('bubble', {
+  template: `<div class="stat-bubble" :class="type + '-bubble'" :style="bubbleStyle">
+    <img :src="'svg/' + type + '-bubble.svg'" />
+    <div class="counter">{{ value }}</div>
+  </div>`,
+  props: ['type', 'value', 'ratio'],
+  computed: {
+    bubbleStyle () {
+      return {
+        top: (this.ratio * 220 + 40) * state.worldRatio + 'px',
+      }
+    },
+  },
+})
+
+Vue.component('banner-bar', {
+  template: '#banner',
+  props: ['color', 'ratio'],
+  computed: {
+    targetHeight () {
+      return 220 * this.ratio + 40
+    },
+  },
+  data () {
+    return {
+      height: 0,
+    }
+  },
+  watch: {
+    targetHeight (newValue, oldValue) {
+      const vm = this
+      new TWEEN.Tween({ value: oldValue })
+        .easing(TWEEN.Easing.Cubic.InOut)
+        .to({ value: newValue }, 500)
+        .onUpdate(function () {
+          vm.height = this.value.toFixed(0)
+        })
+        .start()
+    },
+  },
+  created () {
+    this.height = this.targetHeight
+  },
+})
+
+// 食物和生命值的气泡
+Vue.component('bubble', {
+    template: `<div class="stat-bubble" :class="type + '-bubble'" :style="bubbleStyle">
+      <img :src="'svg/' + type + '-bubble.svg'" />
+      <div class="counter">{{ value }}</div>
+    </div>`,
+    props: ['type', 'value', 'ratio'],
+    computed: {
+      bubbleStyle () {
+        return {
+          top: (this.ratio * 220 + 40) * state.worldRatio + 'px',
+        }
+      },
+    },
+})
+
+// 旗帜栏
+Vue.component('banner-bar', {
+    template: '#banner',
+    props: ['color', 'ratio'],
+    computed: {
+      targetHeight () {
+        return 220 * this.ratio + 40
+      },
+    },
+    data () {
+      return {
+        height: 0,
+      }
+    },
+    watch: {
+      targetHeight (newValue, oldValue) {
+        const vm = this
+        new TWEEN.Tween({ value: oldValue })
+          .easing(TWEEN.Easing.Cubic.InOut)
+          .to({ value: newValue }, 500)
+          .onUpdate(function () {
+            vm.height = this.value.toFixed(0)
+          })
+          .start()
+      },
+    },
+    created () {
+      this.height = this.targetHeight
+    },
+})
+  
