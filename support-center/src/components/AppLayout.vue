f825fe441c7e4264f1b57e53768406634852c366
Rachelmu <631032057@qq.com>
2020-09-08 19:55:27 +0800

    support-center comm

--- support-center/src/components/AppLayout.vue
+++ support-center/src/components/AppLayout.vue
@@ -11,3 +11,14 @@
         <router-view />
     </div>
 </template>
+<style lang="stylus">
+@import '../style/main';
+</style>
+<style lang="stylus" scoped>
+.header{
+    .img{
+        width: 64px;
+        height: 64px;
+    }
+}
+</style>
---
+++ support-center/src/components/Hom.vue
@@ -0,0 +1,5 @@
+<template>
+    <main class="home">
+        <h1>Welcome to our support center</h1>
+    </main>
+</template>
