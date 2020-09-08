72350844df178197443f793b8809732f147c3128
Rachelmu <631032057@qq.com>
2020-09-08 19:43:50 +0800

    support-center assets

---
+++ support-center/src/components/AppLayout.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="app-layout">
+        <header class="header">
+            <div>
+                <img src="../assets/logo.svg" alt="">
+            </div>
+            <div>My shirt shop</div>
+        </header>
+
+        <!-- 菜单将放在这里 -->
+        <router-view />
+    </div>
+</template>
