375cea95cf24a7fdee759fb04f0bc10643a4e2da
Rachelmu <631032057@qq.com>
2020-09-09 15:31:44 +0800

    router

--- support-center/src/components/Hom.vue
+++ support-center/src/components/Hom.vue
@@ -1,5 +1,8 @@
 <template>
     <main class="home">
         <h1>Welcome to our support center</h1>
+        <p>
+            We are here to help! Pleas read the <a href="">F.A.Q</a>
+        </p>
     </main>
 </template>
