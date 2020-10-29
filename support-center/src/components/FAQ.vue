bd324ddd6d43cbab6cf2500aeb7a3d465b6287e2
Rachelmu <631032057@qq.com>
2020-10-29 10:53:59 +0800

    服务器

--- support-center/src/components/FAQ.vue
+++ support-center/src/components/FAQ.vue
@@ -1,5 +1,24 @@
 <template>
 	<main class="faq">
 		<h1>Frenquently Asked Questions</h1>
+		<div class="error" v-if="error">
+			Can't load the questions
+		</div>
+		<section class="list">
+			<article v-for="question of question" :key="question">
+				<h2 v-html="question.title"></h2>
+				<p v-html="question.content"></p>
+			</article>
+		</section>
 	</main>
 </template>
+<script>
+export default{
+	data(){
+		return{
+			questions: [],
+			error: null
+		}
+	}
+}
+</script>
