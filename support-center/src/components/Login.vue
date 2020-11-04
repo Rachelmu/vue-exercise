2462bfa89a081f467a3ce8a092a1fe1fa6344a6d
Rachelmu <631032057@qq.com>
2020-11-04 10:50:46 +0800

    Login

---
+++ support-center/src/components/Login.vue
@@ -0,0 +1,21 @@
+<template>
+	<main class="login">
+		<h1>Please login to continue</h1>
+		<form>
+			<FormInput
+				name="username"
+				:value="username"
+				placeholder="Username"
+			/>
+		</form>
+	</main>
+</template>
+<script>
+export default{
+	data(){
+		return{
+			username: ''
+		}
+	}
+}
+</script>
